535776d033370e3fa81aab55b78f70124b0ca863
Lorenzo Fabbri <lorenzo.fabbri@isglobal.org>
2023-02-06 16:28:55 +0100

    Kept working on functions for DAGs

--- R/dags.R
+++ R/dags.R
@@ -22,3 +22,41 @@ visualize.dag <- function(dag.code) {
 
   return(plt)
 }
+
+#' Test conditional independencies.
+#' `test.npsem` checks whether the assumptions
+#' encoded in the DAG are consistent with the data.
+#'
+#' @param dag.code Input DAG name. A string.
+#' @param dat The dataset to test the DAG against. A dataframe.
+#' @param params A named list containing eventual parameters.
+#' @returns The results of `dagitty::localTests`.
+#' @importFrom magrittr %>%
+#' @export
+test.npsem <- function(dag.code, dat, params) {
+  # Step 0: create dagitty object
+  dag <- dagitty::dagitty(dag.code) %>%
+    ggdag::tidy_dagitty()
+
+  # Step 1: extract adjustment set(s)
+  dag.as <- dagitty::adjustmentSets(x = dag,
+                                    type = params$type.mas,
+                                    effect = params$type.effect)
+  if (length(dag.as) > 1) {
+    print(dag.as)
+    cat("\n")
+    idx = readline(prompt = "There are multiple adjustment sets. Select one: ") %>%
+      as.integer()
+    dag.as <- dag.as[[idx]]
+  }
+
+  # Step 2: map nodes in DAG to variable names in dataset and extract columns
+  dag.as.clean <- map.covariates(adj.set = dag.as, params = params)
+  dat.test <- dat %>% dplyr::select(dplyr::all_of(dag.as.clean))
+
+  # Step 3: test independencies
+  ret <- dagitty::localTests(x = dag, data = dat.test,
+                             type = "cis.loess", R = 5)
+
+  return(ret)
+}
