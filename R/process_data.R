13f3c3175c249780086aaab88c934e5aca8e0f04
Lorenzo Fabbri <lorenzo.fabbri@isglobal.org>
2023-02-09 12:08:10 +0100

    Kept working on functions for DAGs and data processing

---
+++ R/process_data.R
@@ -0,0 +1,12 @@
+#' Mapping from DAG nodes to variables.
+#' `map.covariates` maps the names of the nodes in a DAG
+#' to the corresponding variables in the dataset.
+#'
+#' @param adj.set .
+#' @param params .
+#' @returns .
+#' @importFrom magrittr %>%
+#' @export
+map.covariates <- function(adj.set, params) {
+
+}
