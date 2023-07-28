0ccdeeb718c8c60abcaa9ed03928c32c58a6b5f2
Lorenzo Fabbri <lorenzo.fabbri@isglobal.org>
2023-07-28 13:52:20 +0200

    Small bug in looping over steps of pre-processing.

--- R/process_data.R
+++ R/process_data.R
@@ -95,7 +95,7 @@ preproc_data <- function(dat, covariates, outcome,
                          id_var, by_var) {
   dat_ret <- dat
 
-  for (step in dic_steps) {
+  for (step in names(dic_steps)) {
     dat_ret <- switch (step,
       "llodq" = handle_llodq(dat = dat_ret,
                              method = dic_steps$llodq$method),
